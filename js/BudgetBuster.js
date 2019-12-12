e18f65a2b82f2ab9948e1cafd89f431e36c5f056
Idris Hobdy <idrishobdy@yahoo.com>
2019-12-12 16:43:42 -0500

    name input added weekly budget in span added but not working

--- js/BudgetBuster.js
+++ js/BudgetBuster.js
@@ -1,4 +1,7 @@
-let weeklyBudget = prompt("Enter your weekly budget",0);
+let weeklyBudget = prompt("Enter your weekly budget", 0);
+document.getElementsByTagName("span").textContent += weeklyBudget;
 
-
-
+function main() {
+let g = document.getElementsByTagName("input")[0].value;
+document.getElementsByTagName("h1")[0].textContent += g;
+}
