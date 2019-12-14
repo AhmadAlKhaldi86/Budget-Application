5d29a1fd7efe5c9c43da53a2658baf4c89d68bf7
Ahmad <aalkhald@ford.com>
2019-12-13 21:46:41 -0500

    adding JavaScript to accept and sum spending

--- js/BudgetBuster.js
+++ js/BudgetBuster.js
@@ -15,3 +15,73 @@ let n = weekday[d.getDay()];
 if (n === weekday[4]) {
     setBudget();
 }
+
+
+
+// This Class is used to create a template for the user expenses 
+class spending {
+    constructor(userName) {
+        this.entSpending =  [];
+        this.foodSpending =  [];
+        this.billsSpending =  [];
+        this.clothingSpending =  [];
+        this.userName = userName;
+      }
+
+    getUser(userName) {
+        return userName;
+        console.log(userName);
+    } 
+
+    addEntSpending(amount) {
+        this.entSpending.push(amount);
+    }
+
+    addFoodSpending(amount) {
+        this.foodSpending.push(amount);
+    }
+
+    addBillsSpending(amount) {
+        this.billsSpending.push(amount);
+    }
+
+    addClothingSpending(amount) {
+        this.clothingSpending.push(amount);
+    }
+
+    getEntSpending() {
+        function addFun(a,b) { return a + b;}
+        const sum = this.entSpending.reduce(addFun);
+        return sum;
+    }
+}
+
+// Creating an object of new user
+// userName = get from input 
+let User = new spending("Ahmad");
+
+// Getting the DOM Elements that will be used to add amount spent to our object. 
+let add = document.getElementById("Add");
+let selectItems = document.getElementById("category");
+function getCategory() {
+    let amountStr = document.getElementById("amount").value;
+    let amount = Number(amountStr);
+    let selectedItem = selectItems.options[selectItems.selectedIndex].value;
+    if (selectedItem === 'food') {
+        User.addFoodSpending(amount);
+        console.log(User.foodSpending);
+    } else if (selectedItem === 'bills') {
+        User.addBillsSpending(amount);
+        console.log(User.billsSpending);
+    } else if (selectedItem === 'ent') {
+        User.addEntSpending(amount);
+        User.getEntSpending();
+        console.log(User.getEntSpending());
+    } else if (selectedItem === 'clothing') {
+        User.addClothingSpending(amount);
+        console.log(User.clothingSpending);
+    }
+}
+
+// Adding an event Listener to trigger every time a user is adding new expense. 
+add.addEventListener("click", getCategory, false);
