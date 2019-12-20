3597fcb033ac90d13b67e0b77006f82d750cf28e
Idris Hobdy <idrishobdy@yahoo.com>
2019-12-19 20:50:05 -0500

    colors on chart changed

--- js/BudgetBuster.js
+++ js/BudgetBuster.js
@@ -189,10 +189,14 @@ function chartUpdates() {
                 label: 'Expense by category',
                 data: [entSpending,clothingSpending,billsSpending,foodSpending],
                 backgroundColor: [
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    // 'rgba(255, 206, 86, 1)',
+                    // 'rgba(75, 192, 192, 1)',
+                    // 'rgba(153, 102, 255, 1)',
+                    // 'rgba(255, 159, 64, 1)'
+                    '#FB62F6',
+                    '#7DDF64',
+                    '#FF4242',
+                    '#B3FFFC'
                 ]
             }]
         },
